cde4a26597568dbf56d3cb2a689ae3b8f50e9bf4
Verdiyanto Saputra <rider.moth@gmail.com>
2018-05-04 19:48:43 +0700

    initial commit

---
+++ src/main/java/MyFirstTelegramBot.java
@@ -0,0 +1,35 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MyFirstTelegramBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "ridermothBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "590566978:AAGSYF9JqvcYQL9J0pEjW4OY5sxPmrzr7cA";
+    }
+}
